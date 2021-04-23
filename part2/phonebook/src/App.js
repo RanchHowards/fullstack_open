8119c2f74b3df48dde91d3d068888e022d224061
Ranch Howards <69303624+RanchHowards@users.noreply.github.com>
2021-04-23 14:28:09 +0200

    set up app to fetch data from json server.  complete 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 import Filter from './Components/Filter'
 import People from './Components/People'
 import PersonForm from './Components/Form'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
@@ -42,7 +39,13 @@ const App = () => {
     setSearchName(event.target.value)
 
   }
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }
 
+  useEffect(hook, []) // the empty array means this efect will only be run after the first render
 
   return (
     <div>
