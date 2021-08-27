00f8b400351ff0d15dd7cf7a4afd2acdf910623a
Elviram <elviram@Elvirams-MacBook-Pro.local>
2021-08-27 15:11:26 +0200

    finish 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState(null)
   const result = useQuery(ALL_BOOKS)
 
   if (!props.show) {
@@ -14,10 +15,19 @@ const Books = (props) => {
 
   const books = result.data.allBooks
 
+  const booksFilter = books.filter((a) =>
+    genre ? a.genres.includes(genre) : a
+  )
+
+  const uniqueGenres = books
+    .map((a) => a.genres) //array of arrays
+    .flat() //flattened into one array
+    .filter((v, i, a) => a.indexOf(v) === i) //filtered for unique values
+
   return (
     <div>
       <h2>books</h2>
-
+      {genre && <div>selected genre = {genre}</div>}
       <table>
         <tbody>
           <tr>
@@ -25,7 +35,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {booksFilter.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -34,6 +44,14 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {uniqueGenres.map((a) => (
+          <button key={a} onClick={() => setGenre(a)}>
+            {a}
+          </button>
+        ))}
+        <button onClick={() => setGenre(null)}>reset</button>
+      </div>
     </div>
   )
 }
